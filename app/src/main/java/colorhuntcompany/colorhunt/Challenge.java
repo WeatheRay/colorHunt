c8a2365861a7bd836ef86ebf502db53d212eda22
WeatheRay <n.a.layfield@gmail.com>
2019-03-09 16:42:17 -0600

    random color generated on the challenge page

--- app/src/main/java/colorhuntcompany/colorhunt/Challenge.java
+++ app/src/main/java/colorhuntcompany/colorhunt/Challenge.java
@@ -18,7 +18,12 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
+
 import android.graphics.Bitmap;
+import android.graphics.Bitmap;
+
+import static android.graphics.Color.rgb;
 
 public class Challenge extends AppCompatActivity {
 
@@ -46,12 +51,12 @@ public class Challenge extends AppCompatActivity {
         Bitmap mImageBitmap = (Bitmap) extras.get("data");
         //ImageView mImageView=findViewById(R.id.mImageView);
         //mImageView.setImageBitmap(mImageBitmap);
-        compress(mImageBitmap);
+        compress(mImageBitmap, "image");
     }
-    public void compress( Bitmap bitmap)
+    public void compress( Bitmap bitmap,String data)
     {
         try {
-            FileOutputStream fout = new FileOutputStream(getFilesDir().toString()+"image.jpeg");
+            FileOutputStream fout = new FileOutputStream(getFilesDir().toString()+data+".jpeg");
             //String test = getFilesDir().toString();
 
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fout);
@@ -89,6 +94,15 @@ public class Challenge extends AppCompatActivity {
         setContentView(R.layout.activity_challenge);
         // /data/user/0/colorhuntcompany.colorhunt/files
         //String test = getFilesDir().toString();
+        Bitmap image = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+        int max = 255;
+        int min = 0;
+        // create instance of Random class
+        Random randomNum = new Random();
+        image.eraseColor(rgb(randomNum.nextInt(max-min),randomNum.nextInt(max-min),randomNum.nextInt(max-min)));
+        ImageView mImageView=findViewById(R.id.mImageView);
+        mImageView.setImageBitmap(image);
+        compress(image, "target.jpeg");
     }
 
 }
