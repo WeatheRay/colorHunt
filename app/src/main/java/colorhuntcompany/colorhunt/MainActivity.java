59df1677d6b02781b954e1ad1517c4c11ddd4bee
WeatheRay <n.a.layfield@gmail.com>
2019-03-09 14:50:27 -0600

    added challenge page

--- app/src/main/java/colorhuntcompany/colorhunt/MainActivity.java
+++ app/src/main/java/colorhuntcompany/colorhunt/MainActivity.java
@@ -1,44 +1,40 @@
 package colorhuntcompany.colorhunt;
 
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
+    public static Activity fa;
+    public void challengeButton(View v){
+            startActivity(new Intent(MainActivity.this, Challenge.class));
         }
-    };
+
+
+
+
+                /*
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        getApplicationContext().getPackageName() + ".provider",
+                        photoFile);
+                        */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+            fa = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //dispatchTakePictureIntent(1);
+        // /data/user/0/colorhuntcompany.colorhunt/files
+        String test = getFilesDir().toString();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        int count=0;
     }
+
 
 }
